a28500edec9572a7a13e0d90f34d87c7c5116546
luke <gksdnr89@gamil.com>
2020-06-26 09:36:14 +0900

    initial commit

---
+++ libraries/custom-components/build/services/custom-components/components/Button/index.js
@@ -0,0 +1,19 @@
+import React$1 from 'react';
+import classnames from '../../../../node_modules/classnames/index.js';
+import styles from './style.module.scss.js';
+
+var ButtonType;
+(function (ButtonType) {
+    ButtonType["DEFAULT"] = "default";
+    ButtonType["PRIMARY"] = "primary";
+    ButtonType["SECONDARY"] = "secondary";
+})(ButtonType || (ButtonType = {}));
+var Button = function (_a) {
+    var children = _a.children, _b = _a.theme, theme = _b === void 0 ? ButtonType.DEFAULT : _b;
+    var classNameProps = classnames(styles.default, styles[theme]);
+    return React$1.createElement("button", { className: classNameProps }, children);
+};
+
+export default Button;
+export { ButtonType };
+//# sourceMappingURL=index.js.map
